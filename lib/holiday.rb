5b2b5ff6944f4aa9b862e5647117d87542290018
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-04 03:21:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_supplies)
 end
 
 def all_holidays_with_bbq(holiday_supplies)
-  holiday_supplies.values("BBQ")
+  holiday_supplies.values == "BBQ"
   binding.pry
 end
 
