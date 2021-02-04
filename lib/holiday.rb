7fa5cec15b8117127ba62d5b2ac04139e3abbee1
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-04 02:30:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ end
 
 def all_supplies_in_holidays(holiday_supplies)
   holiday_supplies.each do |season, holiday_hash|
-    season.to_s.capitalize!.insert(-1, ":")
+    puts season.to_s.capitalize!.insert(-1, ":")
     holiday_hash.each do |holiday, supply|
     end
   end
