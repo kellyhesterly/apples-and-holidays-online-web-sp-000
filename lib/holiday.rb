a0d4b566c6f90d40bd066451d6ef93635a62c913
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-04 02:57:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,12 +61,12 @@ end
 def all_supplies_in_holidays(holiday_supplies)
   holiday_supplies.each do |season, holiday_hash|
     puts season.to_s.capitalize!.insert(-1, ":")
-    holiday_hash.each do |holiday, supply|
+    holiday_hash.each do |holiday|
       holiday_array = holiday.to_s.split("_")
       cap_holiday = holiday_array.map do |word|
         word.capitalize
     end.join(" ")
-    puts cap_holiday.insert(-1, ":")
+    puts cap_holiday.insert(-1, ":").
     end
   end
 end
